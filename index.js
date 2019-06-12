63670063e72960467b9e53c06965926ad7b735c4
unknown <jaziel.moran@doitcloud.consulting>
2019-06-12 15:44:12 -0500

    intento 1.3 12-06-2019

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-/*app.get('/', function(req, res){
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-});*/
+});
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
-  });
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message2', function(msg){
-    io.emit('chat message2', msg);
   });
 });
 
