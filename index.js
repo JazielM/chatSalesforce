90259a29e9922ec1ce0f21a7c41716dff089156c
unknown <jaziel.moran@doitcloud.consulting>
2019-06-14 12:40:15 -0500

    guardado JSON v1.0.0

--- index.js
+++ index.js
@@ -7,45 +7,37 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var conversation_json = {"id_conversation" : "0051U000002CgnKQAS:0051U000005ujBxQAI"};
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-     var temp = JSON.parse(msg);
-    io.emit(temp.currentUserId+':'+temp.usuarioId, msg);
+  socket.on('chat message', function(return_data){
+
+    var data = JSON.parse(return_data);
+
+    /* Aquí :: Verificar si existe la conversación */
+
+    /* ----- */
+
+    /* Comparamos si existe el */
+    if("messages" in data){
+      conversation_json["messages"].push({
+        "id_message" : conversation_json["id_conversation"] + ":" + conversation_json["messages"].length+1,
+        "body" : data.msg,
+        "sender_name" : data.currentUserName,
+        "date" : data.date,
+        "time" : data.time
+      }); 
+
+    }
+
+    
+    io.emit(data.currentUserId+':'+data.usuarioId, return_data);
   });
 });
+
+function imprime_conversacion(){
+  console.log(conversation_json);
+}
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-/*const express     = require('express');
-const path        = require('path');
-const PORT        = process.env.PORT || 5000;
-const bodyParser  = require('body-parser');
-
-
-var app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-var server  = require('http').Server(app);
-var io    = require('socket.io')(server, { origins: '*:*'});
-
-var request = require('request');
-
-
-
-app.post('/catchedMessages', function (req, res) {
-
-  
-  res.setHeader('Content-Type', 'application/json');
-  //if(req.body.messages[0].author.indexOf('5581047059') < 0 || req.body.messages[0].author.indexOf('5564030776') < 0 || req.body.messages[0].senderName.indexOf('Banda') < 0 || req.body.messages[0].author.indexOf('5583492958') < 0  || req.body.messages[0].author.indexOf('2351116012') < 0  || req.body.messages[0].author.indexOf('5523371751') < 0 ){
-    //io.emit('5569648556', JSON.stringify(req.body, null, 2));
-    //io.emit('5569648556', JSON.parse(req.body.messages[0].body));
-  //}
-
-  io.emit('chat message', req.body);
-
-
-
-  res.end(JSON.stringify(req.body, null, 2));
-});*/
