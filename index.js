02ba461322743f70ae94a6ee4eb5b30bfd297718
unknown <jaziel.moran@doitcloud.consulting>
2019-06-14 15:47:46 -0500

    imprimir file v1.0.4

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var fs = require('fs');
+var path = require('path');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
