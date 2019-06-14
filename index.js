647a16aa52d6065b497edf2e9bb8fd6da88cdebf
unknown <jaziel.moran@doitcloud.consulting>
2019-06-14 16:16:07 -0500

    imprimir file v1.0.6

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var conversation_json = {"id_conversation" : "0051U000002CgnKQAS:0051U000005ujBxQAI"};
+//var conversation_json = {"id_conversation" : "0051U000002CgnKQAS:0051U000005ujBxQAI"};
 io.on('connection', function(socket){
   socket.on('chat message', function(return_data){
 
@@ -19,7 +19,7 @@ io.on('connection', function(socket){
     /* ----- */
 
     /* Comparamos si existe el */
-    if("messages" in data){
+    /*if("messages" in data){
       conversation_json["messages"].push({
         "id_message" : conversation_json["id_conversation"] + ":" + (conversation_json["messages"].length+1),
         "body" : data.msg,
@@ -35,7 +35,42 @@ io.on('connection', function(socket){
         "date" : data.date,
         "time" : data.time
       }; 
-    }
+    }*/
+
+    fs.open('mynewfile2.txt', 'w', function (err, file) {
+      if (err) {
+        //data["imprime"] = "No Existe el archivo";
+        throw err;
+      }
+      var conversation_json =  JSON.parse(file);
+
+      if(file.length == 0){
+        var id_conversation = data.currentUserId+':'+data.usuarioId;
+
+        conversation_json["id_conversation"] = id_conversation
+        conversation_json["messages"] = {
+          "id_message" : id_conversation + ":" + 0,
+          "body" : data.msg,
+          "sender_name" : data.currentUserName,
+          "date" : data.date,
+          "time" : data.time
+        }; 
+      }else{
+        conversation_json["messages"].push({
+          "id_message" : conversation_json["id_conversation"] + ":" + (conversation_json["messages"].length+1),
+          "body" : data.msg,
+          "sender_name" : data.currentUserName,
+          "date" : data.date,
+          "time" : data.time
+        }); 
+      }
+      file = JSON.stringify(conversation_json);
+      data["imprime"] = conversation_json;
+      //var conversation =  JSON.parse(file);
+
+      //data["imprime"] = "Existe el archivo";
+      console.log('Saved!');
+    });
 
     /*fs.access("Jaz.txt", fs.F_OK, (err) => {
       if (err) {
